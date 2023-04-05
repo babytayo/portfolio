fe4786a693d10ada4f246abc4490670fd84b69de
babytayo <121583836+babytayo@users.noreply.github.com>
2023-04-06 00:11:31 +0100

    added js

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+//Readmore btns onclick function
+function myfunction1() {
+  document.getElementById("read1").style.display = "block";
+}
+
+function myfunction2() {
+  document.getElementById("read2").style.display = "block";
+}
+
+function myfunction3() {
+  document.getElementById("read3").style.display = "block";
+}
