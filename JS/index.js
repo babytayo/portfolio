99e8246a289a5b03acf54ef16e69d7998265e3cf
babytayo <121583836+babytayo@users.noreply.github.com>
2023-04-30 11:30:56 +0100

    work in progress

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,38 @@
+function hamburgerMenu() {
+  let list = document.getElementById("navMenu");
+  if (list.style.display == "none") {
+    list.style.display = "block";
+  } else {
+    list.style.display = "none";
+  }
+}
+
 //Readmore btns onclick function
 function myfunction1() {
-  document.getElementById("read1").style.display = "block";
+  let readMore = document.getElementById("read1");
+  if (readMore.style.display == "block") {
+    readMore.style.display = "none";
+  } else {
+    readMore.style.display = "block";
+  }
 }
 
 function myfunction2() {
-  document.getElementById("read2").style.display = "block";
+  let readMore2 = document.getElementById("read2");
+  if (readMore2.style.display == "block") {
+    readMore2.style.display = "none";
+  } else {
+    readMore2.style.display = "block";
+  }
 }
 
 function myfunction3() {
-  document.getElementById("read3").style.display = "block";
+  let readMore3 = document.getElementById("read3");
+  if (readMore3.style.display == "block") {
+    readMore3.style.display = "none";
+  } else {
+    readMore3.style.display = "block";
+  }
 }
 
 //whatsapp me
